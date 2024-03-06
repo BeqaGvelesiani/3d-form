f5620b1f34892168ad2af736acab1ca049831acc
beshqen <beqagvelesiani1990@gmail.com>
2024-03-07 02:46:03 +0400

    dxcesdx

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const signInForm = document.getElementById("signInForm");
 const signUpForm = document.getElementById("signUpForm");
 const prop = document.documentElement.style;
 let tougleIndicator = true;
+let rotationTime = 1000;
+
+
+prop.setProperty("--rotationTime", `${rotationTime}ms`);
 
 let rotation1 = 0;
-let rotation2 = 180;
+let rotation2 = 0.5;
+
+signUpForm.style.transform = `rotateY(${rotation2}turn)`;
 
 tougle.addEventListener("click", () => {
   if (tougleIndicator) {
@@ -18,46 +24,23 @@ tougle.addEventListener("click", () => {
 function toSignUp() {
   prop.setProperty("--positionX", "calc(var(--tougleHeight) * 2)");
   prop.setProperty("--rotate", "rotate(-410deg)");
-  hideSignInForm();
-  setTimeout(function () {
-    ShowSignUpForm();
-  }, 5000);
+  right();
   tougleIndicator = false;
 }
 
-// function toLogIn() {
-//   prop.setProperty("--positionX", "calc(var(--tougleHeight) * -0.75)");
-//   prop.setProperty("--rotate", "rotate(-130deg)");
-//   //signUpForm.style.transform = "rotateY(180deg)";
-//   //ShowSignInForm();
-//   //hideSignUpForm();
-//   tougleIndicator = true;
-// }
-
-function hideSignInForm() {
-  console.log("start!");
-  signInForm.style.transform = `rotateY(${(rotation1 += 180)}deg)`;
-  setTimeout(function () {
-    signInForm.style.display = "none";
-    console.log(`rotation1 =  ${rotation1} end`);
-  }, 5000);
+function toLogIn() {
+  prop.setProperty("--positionX", "calc(var(--tougleHeight) * -0.75)");
+  prop.setProperty("--rotate", "rotate(-130deg)");
+  left();
+  tougleIndicator = true;
 }
 
-function ShowSignUpForm() {
-  signUpForm.style.display = "grid";
-  console.log("now!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-  signUpForm.style.transform = `rotateY(${(rotation2 += 180)}deg)`;
-  console.log(`rotation2 =  ${rotation2}`);
+function right() {
+  signInForm.style.transform = `rotateY(${(rotation1 += 0.5)}turn)`;
+  signUpForm.style.transform = `rotateY(${(rotation2 += 0.5)}turn)`;
 }
 
-// function ShowSignInForm() {
-//   signInForm.style.transform = `rotateY(${(rotation1 += 180)}deg)`;
-//   signInForm.style.display = "grid";
-//   console.log(`rotation =  ${rotation1}`);
-// }
-
-// function hideSignUpForm() {
-//   signInForm.style.transform = `rotateY(${(rotation2 += 180)}deg)`;
-//   signUpForm.style.display = "none";
-//   console.log(`rotation =  ${rotation2}`);
-// }
+function left() {
+  signUpForm.style.transform = `rotateY(${(rotation2 -= 0.5)}turn)`;
+  signInForm.style.transform = `rotateY(${(rotation1 -= 0.5)}turn)`;
+}
