983ab7728610bab7122747f42ef9c5f1a4ae40b3
beshqen <beqagvelesiani1990@gmail.com>
2024-03-08 00:18:48 +0400

    swedf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const tougle = document.getElementById("tougle");
 const form = document.getElementById("form");
 const prop = document.documentElement.style;
-
+const body = document.getElementById("body");
 let tougleIndicator = true;
 
 let rotation = 0;
@@ -29,3 +29,32 @@ function toLogIn() {
   prop.setProperty("--rotate", "rotate(-130deg)");
   tougleIndicator = true;
 }
+
+function divadd(a, b, c, d) {
+  const div = document.createElement("div");
+  div.className = "snow";
+  div.style.left = `${random(a, b)}px`;
+  div.style.top = `${random(c, d)}px`;
+  body.appendChild(div);
+  setInterval(function () {
+    div.style.transform = "translateY(-107vh)";
+    div.style.transition = "transform 10s";
+  });
+}
+
+function random(a, b) {
+  let i = b - a + 1;
+  return Math.floor(Math.random() * i) + a;
+}
+
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
+divadd(1, 300, 1, 700);
