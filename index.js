09a3f0db76ae98d73a3bad02a9bd6cacd11c5366
BeqaGvelesiani <107481738+BeqaGvelesiani@users.noreply.github.com>
2024-03-07 17:40:09 +0400

    swdfgdsa

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
 const tougle = document.getElementById("tougle");
-const signInForm = document.getElementById("signInForm");
-const signUpForm = document.getElementById("signUpForm");
+const form = document.getElementById("form");
 const prop = document.documentElement.style;
+
 let tougleIndicator = true;
-let rotationTime = 1000;
 
-let rotation1 = 0;
-let rotation2 = 0.5;
-
-signUpForm.style.transform = `rotateY(${rotation2}turn)`;
+let rotation = 0;
 
 tougle.addEventListener("click", () => {
-  prop.setProperty("--rotationTime", `${rotationTime}ms`);
   if (tougleIndicator) {
     toSignUp();
+    form.style.transform = `rotateY(${(rotation += 0.5)}turn)`;
+    let tougleIndicator = false;
   } else {
     toLogIn();
+    form.style.transform = `rotateY(${(rotation -= 0.5)}turn)`;
+    let tougleIndicator = true;
   }
 });
 
 function toSignUp() {
   prop.setProperty("--positionX", "calc(var(--tougleHeight) * 2)");
   prop.setProperty("--rotate", "rotate(-410deg)");
-  right();
   tougleIndicator = false;
 }
 
 function toLogIn() {
   prop.setProperty("--positionX", "calc(var(--tougleHeight) * -0.75)");
   prop.setProperty("--rotate", "rotate(-130deg)");
-  left();
   tougleIndicator = true;
-}
-
-function right() {
-  signInForm.style.transform = `rotateY(${(rotation1 += 0.5)}turn)`;
-  signUpForm.style.transform = `rotateY(${(rotation2 += 0.5)}turn)`;
-}
-
-function left() {
-  signUpForm.style.transform = `rotateY(${(rotation2 -= 0.5)}turn)`;
-  signInForm.style.transform = `rotateY(${(rotation1 -= 0.5)}turn)`;
 }
